e44dcaabec87333f1b8229bd2266f45d0d347faf
Arthuthu <arthurgeromello@hotmail.com>
2022-10-04 14:32:04 -0300

    Add project files.

---
+++ ConsoleUI/ContainerConfig.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Application>().As<IApplication>();
+            builder.RegisterType<BusinessLogic>().As<IBusinessLogic>();
+
+            //In the DemoLibrary namespace find the word Utilities (folder)
+            //In the utilities folder give me all the classes, register them and then link them up to the matching interface.
+            builder.RegisterAssemblyTypes(Assembly.Load(nameof(DemoLibrary)))
+                .Where(t => t.Namespace.Contains("Utilities"))
+                .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name));
+
+            return builder.Build();
+        }
+    }
+}
